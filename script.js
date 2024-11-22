3a927000855fd2555e73621cd2298bcdfa9059db
Caz-Basso <basso.c985@unesc.net>
2024-11-22 16:36:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var moldura;
+var botaoVoltar;
+var botaoAvancar;
+var indice = 0; // Inicializando o índice com 0 (primeira imagem)
+var temporizador;
+var fotos;
+const pastaImagem = "Imagens/";
+const extensaoImagem = ".jpg";
+
+botaoVoltar = document.getElementById("btn_voltar");
+botaoAvancar = document.getElementById("btn_avancar");
+
+moldura = document.getElementById("moldura");
+
+fotos = ["foto1", "foto2", "foto3", "foto4", "foto5", "foto6", "foto7", "foto8", "foto9", "foto10"];
+
+window.onload = Inicio;
+
+function Inicio() {
+    MostrarFoto(); // Mostrar a primeira foto ao carregar a página
+    botaoAvancar.onclick = Avancar;
+    botaoVoltar.onclick = Voltar;
+}
+
+function MostrarFoto() {
+    moldura.src = pastaImagem + fotos[indice] + extensaoImagem;
+}
+
+function Avancar() {
+    if (indice < fotos.length - 1) {
+        indice++;
+    } else {
+        indice = 0;
+    }
+    MostrarFoto();
+    console.log(indice);
+}
+
+function Voltar() {
+    if (indice > 0) {
+        indice--;
+    } else {
+        indice = fotos.length - 1; // Volta para a última foto
+    }
+    MostrarFoto();
+    console.log(indice);
+}
+
+
+const navEl = document.querySelector('.nav');
+const hamburguerEl = document.querySelector('.hamburguer');
+
+hamburguerEl.addEventListener('click', () => {
+    console.log("Hamburguer clicado!");
+    navEl.classList.toggle('nav--open');
+});
